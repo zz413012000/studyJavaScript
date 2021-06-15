f5cd1ee8c61ff06520c63b689a0b694422969fd2
zz413012000 <zz413012000@gmail.com>
2021-06-16 07:59:34 +0800

    Shadow DOM

---
+++ Web-Component/component/MyMain.mjs
@@ -0,0 +1,32 @@
+class MyMain extends HTMLElement{
+    static style=`
+        main{
+            height:200px;line-height:200px;
+            text-align:center;
+            font-size:1.2rem;font-weight:bold;
+            background-color:#ddd;
+        }
+    `;
+    constructor(){
+        super();
+        this.attachShadow({mode:"open"});
+        // 建立 CSS 樣式
+        this.styling();
+        // render HTML 結構
+        this.render();
+    }
+    styling(){
+        // 1. set up style DOM
+        // 2. assign style textContent
+        // 3. append shadowRoot
+        this.stylesheet=document.createElement("style");
+        this.stylesheet.textContent=this.constructor.style;
+        this.shadowRoot.appendChild(this.stylesheet);
+    }
+    render(){
+        const main=document.createElement("main");
+        main.textContent="Welcome";
+        this.shadowRoot.appendChild(main);
+    }
+}
+export default MyMain;
---
+++ Web-Component/component/MyNav.mjs
@@ -0,0 +1,42 @@
+class MyNav extends HTMLElement{
+    static style=`
+        nav{
+            display:flex;
+        }
+        nav>.title{
+            flex:none;width:200px;
+        }
+        nav>.menu{
+            flex:auto;text-align:right;
+        }
+    `;
+    constructor(){
+        super();
+        this.attachShadow({mode:"open"});
+        // 建立 CSS 樣式
+        this.styling();
+        // render HTML 結構
+        this.render();
+    }
+    styling(){
+        // 1. set up style DOM
+        // 2. assign style textContent
+        // 3. append shadowRoot
+        this.stylesheet=document.createElement("style");
+        this.stylesheet.textContent=this.constructor.style;
+        this.shadowRoot.appendChild(this.stylesheet);
+    }
+    render(){
+        const nav=document.createElement("nav");
+        const title=document.createElement("div");
+        title.className="title";
+        title.textContent="My Web";
+        const menu=document.createElement("div");
+        menu.className="menu";
+        menu.textContent="Web Menu";
+        nav.appendChild(title);
+        nav.appendChild(menu);
+        this.shadowRoot.appendChild(nav);
+    }
+}
+export default MyNav;
